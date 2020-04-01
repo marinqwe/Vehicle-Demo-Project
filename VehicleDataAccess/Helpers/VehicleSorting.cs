cb6173e8132207e3c0918794e55673269a012897
marin <marinmarinbadrov@gmail.com>
2020-04-01 14:03:12 +0200

    Vehicle Model sorting by id fix

--- VehicleDataAccess/Helpers/VehicleSorting.cs
+++ VehicleDataAccess/Helpers/VehicleSorting.cs
@@ -7,13 +7,14 @@ namespace VehicleDataAccess.Helpers
         public string SortBy { get; set; }
         public string SortByName { get; set; }
         public string SortByAbrv { get; set; }
-        public string OrderVehiclesBy { get; set; }
+        public string SortById { get; set; }
 
         public VehicleSorting(string sortBy)
         {
             SortBy = sortBy;
             SortByName = String.IsNullOrEmpty(sortBy) ? "name_desc" : "";
             SortByAbrv = sortBy == "Abrv" ? "abrv_desc" : "Abrv";
+            SortById = sortBy == "MakeId" ? "makeid_desc" : "MakeId";
         }
     }
 }
